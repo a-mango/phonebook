df2324b700ab3ecd756cd3634f3949d7ae4c7f45
Aubry Mangold <aubry.mangold@outlook.com>
2020-08-11 19:37:37 +0200

    uibuild

---
+++ build/precache-manifest.91bd2d0c793ca4158cf92b79ee841a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18bc575a9f3266eb5528c8e0137e859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f30a2a91c08915c0c1",
+    "url": "/static/css/main.c33ca91b.chunk.css"
+  },
+  {
+    "revision": "fa356a675dbfe0bba9ab",
+    "url": "/static/js/2.4541878e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4541878e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f30a2a91c08915c0c1",
+    "url": "/static/js/main.334c9352.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
