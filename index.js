31e314c68c7d4f2d5ffc9ab290e2fb2d34c25e2a
Aubry Mangold <aubry.mangold@outlook.com>
2020-09-24 14:03:25 +0200

    Modify put route to validate on update

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get("/info", (request, response, next) => {
  */
 app.get("/api/persons", (request, response, next) => {
   Person.find({})
-    .then((persons) => persons.map((person) => person.toJSON))
+    .then((persons) => persons.map((person) => person.toJSON()))
     .then((formattedPersons) => {
       response.json(formattedPersons);
     })
@@ -126,7 +126,7 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then((updatedPerson) => updatedPerson.toJSON())
     .then((updatedAndFormattedPerson) => {
       response.json(updatedAndFormattedPerson);
